07ffdf31927c9a7a2308976248acb035380a238a
Leonardo Fochesato Michelon <49353436+L4ndr@users.noreply.github.com>
2020-05-27 13:53:27 -0300

    arrumar handler para contas privadas

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ api = tweepy.API(auth)
 
 def get_followers(screen_name):
     if (api.get_user(screen_name).protected) == False:
-        print("pegando os seguidores de {}...".format(screen_name))
+        print("pegando os seguidores de @{}...".format(screen_name))
         ids = list()
         for page in tweepy.Cursor(api.followers_ids, screen_name=screen_name).pages():
             ids.extend(page)
@@ -21,7 +21,7 @@ def get_followers(screen_name):
         return ids
     else:
         print("impossivel pegar os seguidores de @{}, conta privada".format(screen_name))
-        return ["Sua conta está privada, considere despriva-la."]
+        return []
 
 idsbf = get_followers(api.me().screen_name) #idsbf = ids dos seguidores do bot
 shuffle(idsbf)
@@ -38,20 +38,19 @@ for idbf in idsbf:
     newf = open("./followers/{}.txt".format(idbf), 'r').read()
     unfs = list()
     try:
+        suspcont = 0
         for follower in oldf:
             if follower not in newf:
                 try:
                     unfs.append("@{}".format(api.get_user(int(follower)).screen_name))
                 except:
-                    unfs.append("[conta suspensa]")
-        supscont = 0
-        for unfind in range(len(unfs)):
-            if unfind == "[conta suspensa]":
-                unfs.pop(unfind)
-                supscont += 1
-        if "[conta suspensa]" in unfs:
-            unfs.append("[{} contas suspensas]".format(supscont))
-        text = "Ninguém deixou de te seguir" if len(unfs)==0 else ("{} pessoas deixaram de te seguir:\n{}".format(len(unfs), "\n".join(unfs)))
+                    suspcont += 1
+        if suspcont>0:
+            unfs.append("[{} contas suspensas]".format(suspcont))
+        if (api.get_user().protected):
+            text = "Sua conta está privada, considere despriva-la."
+        else:
+            text = "Ninguém deixou de te seguir" if len(unfs)==0 else ("{} pessoas deixaram de te seguir:\n{}".format(len(unfs), "\n".join(unfs)))
         api.send_direct_message(recipient_id=int(idbf), text=text) if len(text)<=10000 else (api.send_direct_message(recipient_id=int(idbf), text="ow na moral vc ta quebrando o bot, não é culpa minha vc ter tanto seguidor assimkk"))
     except Exception as err:
         print(err)
